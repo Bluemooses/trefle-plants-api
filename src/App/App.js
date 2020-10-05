12bd987f13e36d62d73eecb791fd97379f060b40
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-10-05 15:15:09 -0500

    Call for character names

--- src/App/App.js
+++ src/App/App.js
@@ -1,44 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "semantic-ui-react";
+import { Button, Input } from "semantic-ui-react";
 import axios from "axios";
 import md5 from "md5";
 
-const puke = (obj) => JSON.stringify(obj, null, 2); // this just prints pretty JSON.
-const root = document.getElementById("root"); // empty <pre /> (for JSON formatting)
+
+//requirements for MARVEL API calls
 const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
 const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
 const ts = Date.now();
 const hash = md5(ts + privateKey + publicKey);
 
-const opts = `comics`; // whatever parameters you want, e.g., `characters/1009215`.
 
-const url = `https://gateway.marvel.com/v1/public/${opts}?apikey=${publicKey}&hash=${hash}&ts=${ts}`; // putting it all together
+const opts = `characters`; // whatever parameters you want, e.g., `characters/1009215`.
 
 function App() {
+  const [superHero, setSuperHero] = useState("");
+  const url = `https://gateway.marvel.com/v1/public/${opts}?name=${superHero}&apikey=${publicKey}&hash=${hash}&ts=${ts}`; // putting it all together
+
   const getSupers = () => {
     axios
       .get(url) // library of your choice
-      .then((res) => (root.innerHTML = puke(res))) // process data
+      .then((res) =>
+        console.log(res.data.data.results, res.data, res.data.data)
+      ) // process data
       .catch((err) => console.warn(err)); // handle errors
   };
+
+  const getSuperHero = () => {
+    console.log(superHero);
+    axios
+      .get(url) // library of your choice
+      .then((res) =>
+        console.log(res.data.data.results, res.data, res.data.data)
+      ) // process data
+      .catch((err) => console.warn(err)); // handle errors
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button onClick={getSupers}>getSupers</Button>
+        <Input
+          placeholder="Input a hero"
+          onChange={(e) => setSuperHero(e.target.value)}
+        />
+        <Button onClick={getSuperHero}>Get Marvel Hero</Button>
       </header>
     </div>
   );
