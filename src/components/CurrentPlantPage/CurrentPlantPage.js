6808ce56883168d933ad9edcd39e4132a3cb3091
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-10-12 16:27:19 -0500

    Added basic styling and use of more data from trefle api

--- src/components/CurrentPlantPage/CurrentPlantPage.js
+++ src/components/CurrentPlantPage/CurrentPlantPage.js
@@ -1,17 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import "./CurrentPlantPage.css";
 function CurrentPlantPage(props) {
   //hooks to connect with reducers
   const dispatch = useDispatch();
   const plantReducer = useSelector((state) => state.currentPlantPage);
   const plantData = plantReducer.data;
+  const [windowWid, setWindowWid] = useState(0);
+  const [windowLe, setWindowLe] = useState(0);
+  useEffect(() => {
+    const updateDimensions = () => {
+      let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+      let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+      setWindowLe(windowHeight);
+      setWindowWid(windowWidth);
+
+      console.log(windowWid, windowLe);
+    };
+    window.addEventListener("resize", updateDimensions);
+  }, [windowWid, windowLe]);
 
   return (
     <>
       {plantData ? (
-        <div>
+        <div className="container">
           {plantData.map((plant) => {
-            return <p>{plant.common_name}</p>;
+            return (
+              <div key={plant.id} class="plantDataDiv">
+                <img className="plantImage" src={plant.image_url} alt=""></img>
+                <header>{plant.common_name}</header>
+                <ul>
+                  <li>Scientific Name: {plant.scientific_name}</li>
+                  <li>Year Attributed: {plant.year}</li>
+                  <li>
+                    <button>Plant Details</button>
+                  </li>
+                </ul>
+              </div>
+            );
           })}
         </div>
       ) : (
