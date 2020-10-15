51a0f2bf9fd5b7613702b50eb68714a1e8820673
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-10-15 14:04:48 -0500

    Linked up previous page functionality

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -9,10 +9,14 @@ function Pagination(props) {
     console.log(pages.next);
     dispatch({ type: "GET_NEXT_SEARCH_PAGE", payload: pages.next });
   }
+  function goToPreviousPage() {
+    console.log(pages.prev);
+    dispatch({ type: "GET_NEXT_SEARCH_PAGE", payload: pages.prev });
+  }
 
   return (
     <div>
-      <button>Previous Page</button>
+      <button onClick={goToPreviousPage}>Previous Page</button>
       <p></p>
       <button onClick={goToNextPage}>Next Page</button>
     </div>
