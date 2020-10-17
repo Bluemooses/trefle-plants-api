c1e938119a9f82d6df890bf64130c1bd9900b35d
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-10-17 14:54:45 -0500

    Added build

--- server/server.js
+++ server/server.js
@@ -14,14 +14,6 @@ const plantDetailsRouter = require("./routes/plantDetails.router");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 //cors for connecting to APIs
 app.use(cors());
 
