3b0648c7590940f25e83ea50174aab8bd03815e6
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-10-17 14:39:46 -0500

    Updated server code

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const path = require("path");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 //Routes included
 const callingRouter = require("./routes/calling.router");
@@ -15,11 +15,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
+  //set static folder
   app.use(express.static("client/build"));
 }
-
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 //cors for connecting to APIs
